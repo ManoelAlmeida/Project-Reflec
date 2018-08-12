ad098a4ff8ff3d46d747bf15f313543aec3e9ae1
adoniasluna <alps@ecomp.poli.br>
2018-08-12 16:11:37 -0300

    setting boundaries for tests

--- experiments/utils.py
+++ experiments/utils.py
@@ -4,7 +4,7 @@ class Configuration():
     def __init__(self):
         #self.dimensions = [(-10, 10), (-5, 5), (-20, 20), (-15, 15)]
         #self.dimensions = [(-100, 100), (-50, 50), (-250, 250), (-150, 150)]
-        self.dimensions = [(-10,10),(-10,10)]
+        self.dimensions = [(-60.261, -27.833),(-9.4685,1.95)]
 
         self.num_particles = 30
         self.max_iterations = 100
